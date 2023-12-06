47c8fb499f64791128588190812bec013a1ef0cd
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2023-12-06 08:53:11 +0200

    Add day4 part2

--- src/day4.rs
+++ src/day4.rs
@@ -66,12 +66,40 @@ fn part1(reader: BufReader<File>) {
         .map(|len| (2 as usize).pow((len - 1).try_into().unwrap()))
         .collect();
     let sum: usize = winning_numbers.into_iter().sum();
+    println!("Part1: {:?}", sum);
+}
+
+fn part2(reader: BufReader<File>) {
+    let card_games: Vec<Cards> = reader
+        .lines()
+        .map(|line| Cards::from_line(line.unwrap()))
+        .collect();
+    let winning_numbers: Vec<usize> = card_games
+        .into_iter()
+        .map(|cards| cards.winning_numbers_included())
+        .map(|cards| cards.len())
+        .collect();
+
+    let mut card_counts = vec![1; winning_numbers.len()];
+    for (i, winning_num) in winning_numbers.iter().enumerate() {
+        let copy_from = i + 1;
+        let copy_until = (i + 1) + *winning_num;
+        for j in copy_from..copy_until {
+            if j > card_counts.len() {
+                break;
+            }
+            card_counts[j] += card_counts[i];
+        }
+    }
+    let sum: usize = card_counts.into_iter().sum();
     println!("Part2: {:?}", sum);
 }
 
 pub fn solve() {
     println!("Day 4 solutions:");
     let reader = file_utils::input_reader(INPUT_FILE_PATH);
-    //file_utils::print_reader(reader)
-    part1(reader)
+    part1(reader);
+
+    let reader = file_utils::input_reader(INPUT_FILE_PATH);
+    part2(reader);
 }
